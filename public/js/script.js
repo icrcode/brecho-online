1fe27b9f756da4360e312cf72edb62c3ad828318
icrcode <icarobotelhosocial@gmail.com>
2024-11-22 01:13:31 -0300

    Atualiza o layout e estilo da página inicial da loja de perfumes, adiciona navegação e animações de bolhas

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,18 @@
-document.getElementById('clickMe').addEventListener('click', () => {
-    alert('Você clicou no botão!');
-  });
+document.addEventListener('DOMContentLoaded', () => {
+    const background = document.createElement('div');
+    background.id = 'background';
+    document.body.appendChild(background);
   
+    for (let i = 0; i < 100; i++) {
+      const bubble = document.createElement('div');
+      bubble.className = 'bubble';
+      background.appendChild(bubble);
+    }
+  
+    const bubbles = document.querySelectorAll('.bubble');
+    bubbles.forEach(bubble => {
+      bubble.style.left = `${Math.random() * 100}vw`;
+      bubble.style.animationDuration = `${Math.random() * 5 + 5}s`;
+      bubble.style.animationDelay = `${Math.random() * 5}s`;
+    });
+  });
